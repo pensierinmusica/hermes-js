628d7dcb076a625879b266f7015dd2f47539fa11
Alessandro Zanardi <alessandro@zanardi.org>
2017-09-18 00:22:46 +0200

    build: rename dist files

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import uglify from 'rollup-plugin-uglify';
 const conf = {
   input: 'src/index.js',
   output: {
-    file: 'dist/index.js',
+    file: 'dist/index.umd.js',
     format: 'umd',
     name: 'hermes',
   },
@@ -12,7 +12,7 @@ const conf = {
 };
 
 if (process.env.NODE_ENV === 'production') {
-  conf.output.file = 'dist/index.min.js';
+  conf.output.file = 'dist/index.umd.min.js';
   conf.plugins.push(uglify());
 }
 
